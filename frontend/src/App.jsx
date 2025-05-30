4b9d4a0c6a8d293316937e6b59351c3468fc0939
SujeetRawat24 <sujeetedm8800@gmail.com>
2025-05-30 23:56:13 +0530

    Added signup page UI and protected route endpoints for authenticated users only

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar.jsx";
-import { Routes , Route} from "react-router-dom";
+import { Routes , Route, Navigate} from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
@@ -31,11 +31,11 @@ const App = () => {
       <Navbar/>
 
       { <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/login' element={<LoginPage />} />
+        <Route path='/' element={authUser ?  <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/signup' element={!authUser ?  <SignUpPage /> : <Navigate to="/"/>} />
+        <Route path='/login' element={!authUser ?  <LoginPage /> : <Navigate to="/"/>} />
         <Route path='/settings' element={<SettingsPage />} />
-        <Route path='/profile' element={<ProfilePage />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
 
       </Routes>}
 
