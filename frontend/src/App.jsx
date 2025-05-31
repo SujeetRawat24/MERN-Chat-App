706e45afa9a11a08dbc385c5e4e731cadcb36d63
SujeetRawat24 <sujeetedm8800@gmail.com>
2025-05-31 21:36:22 +0530

    Upated UI & navbar component using react-hot-toast, added signup and logout logic in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import { useAuthStore } from "./store/useAuthStore.js";
 import { useEffect } from "react";
 
 import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -30,15 +31,15 @@ const App = () => {
     <div>
       <Navbar/>
 
-      { <Routes>
+       <Routes>
         <Route path='/' element={authUser ?  <HomePage /> : <Navigate to="/login" />} />
         <Route path='/signup' element={!authUser ?  <SignUpPage /> : <Navigate to="/"/>} />
         <Route path='/login' element={!authUser ?  <LoginPage /> : <Navigate to="/"/>} />
         <Route path='/settings' element={<SettingsPage />} />
         <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-
-      </Routes>}
-
+      </Routes>
+      
+      <Toaster/>
     </div>
     
   );
