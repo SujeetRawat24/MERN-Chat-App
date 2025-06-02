b95a596ed1d69d2721636791329d99e47e3545ac
SujeetRawat24 <sujeetedm8800@gmail.com>
2025-06-02 23:57:49 +0530

    Implemented User profile & login UI and functions logic

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import  toast  from "react-hot-toast";
 export const useAuthStore = create((set) => ({ 
     authUser: null,
     isSigningUp: false,
-    isLogging: false,
+    isLoggingIn: false,
     isUpdatingProfile: false,
 
     isCheckingAuth: true,          //loading state
@@ -45,5 +45,32 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             toast.error(error.response.data.message);
         }
+    },
+
+    login : async(data) => {
+        set({isLoggingIn : true });
+        try {
+            const res = await axiosInstance.post("/auth/login",data);
+            set({ authUser: res.data });
+            toast.success("Login Successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+        finally{
+            set({isLoggingIn : false});
+        }
+    },
+
+    updateProfile : async(data) => {
+        set({isUpdatingProfile : true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }finally {
+            set({ isUpdatingProfile: false });
+          }
     }
 }));
