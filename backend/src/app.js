fedb8913da1b24cfdbcda850ed8d1369ffa7c2c5
SujeetRawat24 <sujeetedm8800@gmail.com>
2025-06-09 23:22:28 +0530

    Project completed && Added Production scripts

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
@@ -12,8 +14,8 @@ import { app, server} from "./lib/socket.js";
 dotenv.config();
 
 
-
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve();
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
@@ -27,6 +29,14 @@ app.use(
 
 app.use('/api/auth/', authRoutes);
 app.use('/api/messages/', messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  })
+}
 
 app.use(errorHandler );
 
